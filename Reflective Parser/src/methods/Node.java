ff0cf0d2245cfef1359168e209ed4b5394ffc7cf
Kyle Perry` <Kyle Perry`@10.13.69.42>
2016-02-26 14:51:20 -0700

    Generated JavaDoc and started Parse method

--- Reflective Parser/src/methods/Node.java
+++ Reflective Parser/src/methods/Node.java
@@ -2,34 +2,64 @@ package methods;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author Kyle Perry
+ *
+ */
 public class Node {
 
 	private ParseGrammer.Expr expression;
 	private ArrayList <Node> sub_expr;
 	
+	/**
+	 * 
+	 * @param expression
+	 */
 	public Node(ParseGrammer.Expr expression) {
 		this.expression = expression;
 		this.sub_expr = new ArrayList<Node>();
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public Object getExpression() {
 		return this.expression;
 	}
 
+	/**
+	 * 
+	 * @param expression
+	 */
 	public void setFunCall(ParseGrammer.Expr expression) {
 		this.expression = expression;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public int numExpressions ()
 	{
 		return sub_expr.size();
 	}
 
+	/**
+	 * 
+	 * @param newExpression
+	 */
 	public void addExpression(Node newExpression)
 	{
 		this.sub_expr.add(newExpression);
 	}
 	
+	/**
+	 * 
+	 * @param index
+	 * @return
+	 */
 	public Node getExpression(int index)
 	{
 		if(sub_expr.isEmpty())
@@ -37,6 +67,9 @@ public class Node {
 		else return null;
 	}
 	
+	/**
+	 * 
+	 */
 	public String toString ()
 	{
 		String nodeString = this.expression.toString();
@@ -47,6 +80,10 @@ public class Node {
 		return nodeString; 
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public int getSize()
 	{
 		int size = 1;
--- Reflective Parser/src/methods/Parser.java
+++ Reflective Parser/src/methods/Parser.java
@@ -1,13 +1,37 @@
 package methods;
 
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Kyle Perry
+ *
+ */
 public class Parser {
 
-	public ParseTree parseLine (String input)
+	/**
+	 * 
+	 * @param input
+	 * @return
+	 * @throws Exception
+	 */
+	public ParseTree parseLine (String input) throws Exception
 	{
 		ParseTree parsedInput = null;
+		Scanner inputScan = new Scanner(input);
+		
 		
 		return parsedInput;
 	}
 	
-	
+/**
+ * 
+ * @param inputScanner
+ * @return
+ * @throws Exception
+ */
+	private Node parseNode (Scanner inputScanner) throws Exception 
+	{	
+		return null;
+	}
 }
